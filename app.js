e3b54640e9024bc27925a887d6687a74fc0458b7
Adarsh Sharma <98509463+AdarshSharma88@users.noreply.github.com>
2024-05-02 00:29:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded({
     extended:true
     
 }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 import course from './routes/courseRoutes.js';
 import user from './routes/userRoutes.js';
 import payment from "./routes/paymentRoutes.js"
