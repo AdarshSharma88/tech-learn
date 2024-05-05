ab317fcb773cd1818feb0871af7b2efd2ee5a124
Adarsh Sharma <98509463+AdarshSharma88@users.noreply.github.com>
2024-05-05 23:36:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-import express from 'express';
-import { config } from 'dotenv';
-import cookieParser from 'cookie-parser';
-import ErrorMiddleware from './middlewares/Error.js';
+import express from "express";
+import { config } from "dotenv";
+import ErrorMiddleware from "./middlewares/Error.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 config({
-    path: "./config/config.env",
-  });
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({
-    extended:true
-    
-}));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
+  path: "./config/config.env",
 });
-import course from './routes/courseRoutes.js';
-import user from './routes/userRoutes.js';
-import payment from "./routes/paymentRoutes.js"
-import people from './routes/peopleRoutes.js';
+const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(cookieParser());
-app.use("/api/v1",user);
-app.use("/api/v1",course);
-app.use("/api/v1",payment);
-app.use("/api/v1",people)
-app.get("/",(req,res)=>res.send(`<h1>Server Working</h1>`))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
+import payment from "./routes/paymentRoutes.js";
+import people from "./routes/peopleRoutes.js";
+
+app.use("/api/v1", course);
+app.use("/api/v1", user);
+app.use("/api/v1", payment);
+app.use("/api/v1", people);
 
 export default app;
+
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
 
 app.use(ErrorMiddleware);
